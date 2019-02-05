11290139b81cf60d17e3e5deff1f53d48c8ca181
hibiki <zfan@iastate.edu>
2019-02-05 00:58:38 -0600

    android experiment redo

---
+++ Experiments/Ming/demo/android/app/src/test/java/com/example/helloworld/ExampleUnitTest.java
@@ -0,0 +1,16 @@
+package com.example.helloworld;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(8, 5 + 3);
+    }
+}
