cd3546c1c711213f9d1753d1b86ddad9fd44dcbc
sgordon4 <sgordon4@iastate.edu>
2019-02-16 19:56:01 -0600

    Completed account creation interface. TODO form http post.

---
+++ Frontend/app/src/main/java/com/example/cs309_cynote/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.cs309_cynote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitForm(View view){
+        System.out.println("Submit pressed - ");
+    }
+}
