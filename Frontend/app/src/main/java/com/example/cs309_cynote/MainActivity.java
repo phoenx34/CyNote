4bc6ee76b549e5c41ec266a544427418bf88a298
sgordon4 <sgordon4@iastate.edu>
2019-02-17 22:32:39 -0600

    Working on api call to send json to server

--- Frontend/app/src/main/java/com/example/cs309_cynote/MainActivity.java
+++ Frontend/app/src/main/java/com/example/cs309_cynote/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.cs309_cynote;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitForm(View view){
-        System.out.println("Submit pressed - ");
+        //System.out.println("Submit pressed - ");
+
+        EditText ACUsername = (EditText) findViewById(R.id.ACUsername); //Find the specific EditText
+        String username = ACUsername.getText().toString().trim();       //Get string, and trim for trailing whitespace
+
+        EditText ACEmail = (EditText) findViewById(R.id.ACEmail);
+        String email = ACEmail.getText().toString().trim();
+
+        EditText ACPassword = (EditText) findViewById(R.id.ACPassword);
+        String password = ACPassword.getText().toString().trim();
+
+        Spinner ACDropdown = (Spinner) findViewById(R.id.ACDropdown);
+        String accType = ACDropdown.getSelectedItem().toString().trim();
+
+        //Throw together the json
+        String json = "{\"username\":\""+username+"\"," +
+                       "\"email\":\""+email+"\", " +
+                       "\"password\":\""+password+"\", " +
+                       "\"type\":\""+accType+"\"}";
+        //Could create a pojo but doesn't really matter
+
+        String testUrl = "http://ptsv2.com/t/mp8ul-1550461405/post";
+
+        System.out.println("Calling API");
+        APICalls api = new APICalls();
+        api.httpPost(testUrl, json);
     }
 }
