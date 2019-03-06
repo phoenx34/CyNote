3248276b2c5694b1ee928db92b745dd9c9b53cbc
sgordon4 <sgordon4@iastate.edu>
2019-03-06 12:10:45 -0600

    Created basic Notes UI

---
+++ Frontend/app/src/main/java/com/example/cs309_cynote/NotesUI.java
@@ -0,0 +1,13 @@
+package com.example.cs309_cynote;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NotesUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
