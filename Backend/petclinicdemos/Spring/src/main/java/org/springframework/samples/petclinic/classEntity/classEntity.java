361b68b1921e6e48f40474892c1e7acc87df9753
misaac34 <misaac34@iastate.edu>
2019-03-09 17:30:10 -0600

    added relationships to class

--- Backend/petclinicdemos/Spring/src/main/java/org/springframework/samples/petclinic/classEntity/classEntity.java
+++ Backend/petclinicdemos/Spring/src/main/java/org/springframework/samples/petclinic/classEntity/classEntity.java
@@ -1,15 +1,20 @@
 package org.springframework.samples.petclinic.classEntity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.NotFound;
 import org.hibernate.annotations.NotFoundAction;
 import org.springframework.core.style.ToStringCreator;
+import org.springframework.samples.petclinic.shoutout.Shoutout;
+import org.springframework.samples.petclinic.textbook.Textbook;
 
 /**
  * 
@@ -24,16 +29,20 @@ public class classEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "CID")    
+    @Column(name = "CID", nullable=false)    
     @NotFound(action = NotFoundAction.IGNORE)
     private Integer CID;        // User ID
 	
     
-    @Column(name = "Name")
+    @Column(name = "Name", nullable=false)
     @NotFound(action = NotFoundAction.IGNORE)
     private String Name;      // Note ID
     
-
+    @OneToMany(mappedBy="CID")
+    private List<Textbook> textbooks;
+    
+    @OneToMany(mappedBy="CID")
+    private List<Shoutout> shoutout;
 
 
     // Getters and Setters below
