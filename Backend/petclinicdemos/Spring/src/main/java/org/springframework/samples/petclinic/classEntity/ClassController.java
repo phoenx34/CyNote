851932a5e2349ba6033f5a3cf3722c793e70d968
misaac34 <misaac34@iastate.edu>
2019-03-10 18:34:30 -0500

    Class creation fixed

--- Backend/petclinicdemos/Spring/src/main/java/org/springframework/samples/petclinic/classEntity/ClassController.java
+++ Backend/petclinicdemos/Spring/src/main/java/org/springframework/samples/petclinic/classEntity/ClassController.java
@@ -1,15 +1,19 @@
 package org.springframework.samples.petclinic.classEntity;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  * 
@@ -25,22 +29,25 @@ public class ClassController {
 
     private final Logger logger = LoggerFactory.getLogger(ClassController.class);
 
-    @RequestMapping(method = RequestMethod.POST, path = "/class/new")
-    public String saveClass(classEntity oneClass) {
-    	classRepository.save(oneClass);
-        return "New class "+ oneClass.getCID() + " Saved";
+    @PostMapping("/classes")
+    public ResponseEntity<Object> saveClass(classEntity oneClass) {
+    	classEntity savedClass = classRepository.save(oneClass);  	
+    	URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}") 			
+    			.buildAndExpand(savedClass.getCID()).toUri();  	
+    	return ResponseEntity.created(location).build();  
+
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/class")
     public List<classEntity> getAllClasses() {
         logger.info("Entered into Controller Layer");
-        List<classEntity> results = classRepository.findAll();
+        List<classEntity> results = (List<classEntity>) classRepository.findAll();
         logger.info("Number of Records Fetched:" + results.size());
         return results;
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/class/{classId}")
-    public Optional<classEntity> findClassById(@PathVariable("classID") String id) {
+    public Optional<classEntity> findClassById(@PathVariable("classID") Integer id) {
         logger.info("Entered into Controller Layer");
         Optional<classEntity> results = classRepository.findById(id);
         return results;
