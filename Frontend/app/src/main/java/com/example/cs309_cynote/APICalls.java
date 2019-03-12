3dc1603654b5813985bde275003216ea21902353
sgordon4 <sgordon4@iastate.edu>
2019-03-11 23:06:45 -0500

    Added get request, fixed post request, added small get test to login page

--- Frontend/app/src/main/java/com/example/cs309_cynote/APICalls.java
+++ Frontend/app/src/main/java/com/example/cs309_cynote/APICalls.java
@@ -1,6 +1,7 @@
 package com.example.cs309_cynote;
 
 import android.app.Application;
+import android.text.TextUtils;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -12,17 +13,24 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * API used to send information to the server, contains several types of HTTP requests
  *
  * @author Sean Gordon
- * @since  2019-02-17
+ * @since 2019-02-17
  */
 public class APICalls extends Application {
     private static APICalls mInstance;
     private RequestQueue requestQueue;
+    private String response = "No response";
 
-    public void onCreate(){
+    public void onCreate() {
         super.onCreate();
         mInstance = this;
     }
@@ -36,75 +44,167 @@ public class APICalls extends Application {
             requestQueue = Volley.newRequestQueue(getApplicationContext());
         return requestQueue;
     }
+
     public void addToRequestQueue(Request request, String tag) {
-        request.setTag(tag);
+        // set the default tag if tag is empty
+        request.setTag(TextUtils.isEmpty(tag) ? APICalls.class.getSimpleName() : tag);
         getRequestQueue().add(request);
     }
-    public void cancelAllRequests(String tag) {
-        getRequestQueue().cancelAll(tag);
+
+    public void addToRequestQueue(Request request) {
+        // set the default tag
+        request.setTag(APICalls.class.getSimpleName());
+        getRequestQueue().add(request);
     }
 
+    public void cancelAllRequests(String tag) {
+        if (requestQueue != null)
+            getRequestQueue().cancelAll(tag);
+    }
+
+
+    //----------------------------------------------------------------------------------------------
+    //                                        Request Bodies
+    //----------------------------------------------------------------------------------------------
+
+
     /**
-     * Simple HTTP post, used to send data to the supplied URL
+     * Volley-based post request, used to send post requests to any formatted urlString and receive
+     * data using Response listeners
      *
-     * @param urlString Url to send data to
-     * @param data      Data to send to the url
+     * @param urlString        String representing the url to send data to
+     * @param data             Data to be packaged with the post request to be sent to the supplied url
+     * @param responseListener Response.Listener, used to handle success responses.
+     *                         Object must be initialized with desired methods before being passed as parameter.
+     *                         Example in Class 'Login', method 'loginToClassSelection'
+     * @param errorListener    Response.ErrorListener, used to handle error responses.
+     *                         Object must be initialized with desired methods before being passed as parameter.
+     *                         Example in Class 'Login', method 'loginToClassSelection'
      */
-    protected void httpPost(String urlString, String data) throws JSONException {
+    protected void volleyPost(String urlString, String data, final Response.Listener<String> responseListener, final Response.ErrorListener errorListener) throws JSONException {
         JSONObject jsonObj = new JSONObject(data);
 
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+        if (responseListener == null)
+            throw new IllegalArgumentException("responseListener null!");
+        if (errorListener == null)
+            throw new IllegalArgumentException("errorListener null!");
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                 Request.Method.POST,
                 urlString,
                 jsonObj,
+
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        System.out.println(response.toString());
-                        //Success Callback
+                        responseListener.onResponse(response.toString());
                     }
                 },
+
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        System.out.println(error.getMessage());
-                        //Failure Callback
+                        errorListener.onErrorResponse(error);
                     }
                 }
         );
 
-        System.out.println("Sending data");
+        System.out.println("Sending data - ");
         System.out.println(data);
-        APICalls.getInstance().addToRequestQueue(jsonObjReq, "postRequest");
+        APICalls.getInstance().addToRequestQueue(jsonObjectRequest, "postRequest");
     }
 
-    //Deprecated, forgot about volley
-    /*protected void httpPost(String urlString, String data){
-        OutputStream out = null;
 
-        System.out.println("Reached API");
 
-        try{
-            System.out.println("Inside API try");
 
-            URL url = new URL(urlString);   //Create a URL object with the url we were given
-            //Set up a connection with the url
-            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-            out = new BufferedOutputStream(connection.getOutputStream());
 
-            BufferedWriter writer = new BufferedWriter(
-                    new OutputStreamWriter(out, "UTF-8")
-            );             //use our BufferedOutputStream and set encoding to UTF-8
 
-            writer.write(data);             //Writes the data
-            writer.close();
+    /**
+     * Volley-based get request, used to send get requests to any formatted urlString and receive
+     * data using Response listeners
+     *
+     * @param urlString        String representing the url to send request to, must include all parameters in the form
+     *                         "sampleUrl.com/endpoint"    or    "sampleUrl.com/endpoint?data1=12345&data2=something"
+     * @param responseListener Response.Listener, used to handle success responses.
+     *                         Object must be initialized with desired methods before being passed as parameter.
+     *                         Example in Class 'Login', method 'loginToClassSelection'
+     * @param errorListener    Response.ErrorListener, used to handle error responses.
+     *                         Object must be initialized with desired methods before being passed as parameter.
+     *                         Example in Class 'Login', method 'loginToClassSelection'
+     */
+    public void volleyGet(String urlString, final Response.Listener<String> responseListener, final Response.ErrorListener errorListener) throws IllegalArgumentException {
 
-            out.close();
+        if (responseListener == null)
+            throw new IllegalArgumentException("responseListener null!");
+        if (errorListener == null)
+            throw new IllegalArgumentException("errorListener null!");
 
-            connection.connect();           //Do some magic
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                urlString,
+                null,
+
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        responseListener.onResponse(response.toString());
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        errorListener.onErrorResponse(error);
+                    }
+                }
+        );
+
+        APICalls.getInstance().addToRequestQueue(jsonObjectRequest, "getRequest");
+    }
+
+
+    /*  I lied volley is easy. Keeping this here for reference
+
+    //I don't have time to learn volley so I'm just forgetting about it
+    protected String httpGet(String urlString, String data) throws IOException {
+
+        //Make a new URL object with the given url, and add any given parameters
+        URL url = new URL(urlString + "?" + data);
+        //Ends up as "sampleUrl.com/endpoint?data1=12345&data2=something"
+        //Data must be in the form of "" or "data1=12345&..."
+
+        System.out.println("\n---------------------------------------\n");
+        System.out.println(url.toString());     //For testing
+
+        //Make a new HttpURLConnection
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        int responseCode = con.getResponseCode();           //Grab the response code
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {    //If success...
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));                 //Read input
+
+            String inputLine;
+            StringBuffer response = new StringBuffer();     //Make a new StringBuffer
+            // to contain response
+            //While there is more reponse to read...
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);                 //Read more response
+            }
+
+            in.close();                     //Close the BufferedReader
+
+            String resp = "{\"code\":\"" + responseCode + "\"," +
+                    "\"data\":" + response.toString() + "}";     //'data' will contain another, nested, json string
+
+            return resp;     //Return the response with the success code
+        } else {
+            return "{\"code\":\"" + responseCode + "\"}";
         }
-        catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-    }*/
+
+    }
+    */
 }
